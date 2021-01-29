d622ce0179e279bed238900cf41cf0e2a61f82b9
cochimu <cochimu1221@icloud.com>
2021-01-29 11:33:13 +0900

    ログインした後トップページに戻る

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -46,42 +46,19 @@ class RegisterController extends Controller
     }
 
     /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-    }
-
-    /**
-     * ユーザー登録後、投稿画面に遷移する
+     * ユーザー登録後、トップページに遷移する
      * @param Request $request
      * @return RedirectResponse
      */
     public function register(Request $request)
     {
+
+        $request->validate([
+            'name' => 'required|max:255|regex:/^[a-zA-Z0-9]+$/',
+            'email' => 'required|max:255|email|unique:users',
+            'password' => 'required|max:255|min:8|regex:/^[a-zA-Z0-9]+$/',
+        ]);
+        
         User::create([
             'name' =>  $request->name,
             'email' => $request->email,
