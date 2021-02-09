346768dc4259a998e41bb15b0770bb8bb20d3291
cochimu <cochimu1221@icloud.com>
2021-02-09 19:20:02 +0900

    deploy

--- backend/resources/views/game/index.blade.php
+++ backend/resources/views/game/index.blade.php
@@ -117,38 +117,6 @@
         @endif
         @endforeach
 
-
-  <div class="table-responsive">
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th>ボードゲーム名</th>
-          <th>プレイ時間</th>
-          <th>最小プレイ人数</th>
-          <th>最大プレイ人数</th>
-          <th>画像</th>
-          <th>詳細</th>
-        </tr>
-      </thead>
-      <tbody id="tb1">
-      @foreach($games as $game)
-      @if($game->image_path)
-        <tr>
-          <td>{{ $game->name }}</td>
-          <td>{{ $game->play_time }}</td>
-          <td>{{ $game->players_minimum }}</td>
-          <td>{{ $game->players_max }}</td>
-          <td><img src="{{ $game->image_path }}"></td>
-          <td class="text-nowrap">
-            <p><a href="{{ route('game.show', $game->id) }}" class="btn btn-primary btn-sm">詳細</a></p>
-          </td>
-        </tr>
-      @endif
-      @endforeach
-      </tbody>
-    </table>
-  </div>
-
       <!-- Footer-->
       <footer class="footer py-4">
             <div class="container">
