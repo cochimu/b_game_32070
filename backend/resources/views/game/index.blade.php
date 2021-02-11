e1dba6975e2cc0acd4dfc0799565f871b1d1129e
cochimu <cochimu1221@icloud.com>
2021-02-11 18:30:25 +0900

    一覧表示、bootstrap変更

--- backend/resources/views/game/index.blade.php
+++ backend/resources/views/game/index.blade.php
@@ -1,11 +1,10 @@
 @extends('common.common')
 
-
 <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="../../../public/img/logo.jpg" alt="" /></a>
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="../../public/img/logo.png" alt="" /></a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ml-1"></i>
@@ -65,65 +64,31 @@
                     <h2 class="section-heading text-uppercase">新着ボードゲーム</h2>
                     <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                 </div>
-                @foreach($games as $game)
-                @if($game->image_path)
+                
                 <div class="row">
+                    @foreach($games as $game)
+                    @if($game->image_path)
                     <div class="col-lg-4 col-sm-6 mb-4">
-                        <div class="portfolio-item">
-                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
-                                <div class="portfolio-hover">
-                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                                </div>
-                                <img src="{{ $game->image_path }}">
+                        <div class="portfolio-item" style="text-align:center">
+                            <a class="portfolio-link" href="{{ route('game.show', $game->id) }}">
+                                <img src="{{ $game->image_path }}" height="200px" width="200">
                             </a>
-                            <div class="portfolio-caption">
+                            <div class="portfolio-caption" style="margin-top:10px">
                                 <div class="portfolio-caption-heading">{{ $game->name }}</div>
                                 <div class="portfolio-caption-subheading text-muted">プレイ時間：{{ $game->play_time }}分</div>
+                                <div class="portfolio-caption-subheading text-muted">人数：{{ $game->players_minimum }}~{{ $game->players_max }}人</div>
                             </div>
                         </div>
                     </div>
+                    @endif
+                    @endforeach
                 </div>
-                @endif
-                @endforeach
+                
             </div>
         </section>
 
-        <!-- Modal -->
-        @foreach($games as $game)
-        @if($game->image_path)
-        <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-8">
-                                <div class="modal-body">
-                                    <!-- Project Details Go Here-->
-                                    <h2 class="text-uppercase">{{ $game->name }}</h2>
-                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                    <img src="{{ $game->image_path }}">
-                                    <p>{{ $game->describe }}</p>
-                                    <ul class="list-inline">
-                                        <li>プレイ時間：{{ $game->play_time }}分</li>
-                                        <li>最小プレイ人数：{{ $game->players_minimum }}</li>
-                                        <li>最大プレイ人数：{{ $game->players_max }}</li>
-                                    </ul>
-                                    <button class="btn btn-primary" data-dismiss="modal" type="button">
-                                        <i class="fas fa-times mr-1"></i>
-                                        閉じる
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        @endif
-        @endforeach
-
-      <!-- Footer-->
-      <footer class="footer py-4">
+        <!-- Footer-->
+        <footer class="footer py-4">
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-4 text-lg-left">Copyright © Your Website 2020</div>
@@ -138,5 +103,5 @@
                     </div>
                 </div>
             </div>
-      </footer>
-</body>
+        </footer>
+<body>
