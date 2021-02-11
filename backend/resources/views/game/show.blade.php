11d1ee8827b084b458c4c948c8d35eb04a4fbf79
cochimu <cochimu1221@icloud.com>
2021-02-11 21:49:52 +0900

    詳細画面変更完了

--- backend/resources/views/game/show.blade.php
+++ backend/resources/views/game/show.blade.php
@@ -2,4 +2,34 @@
 
 @section('content')
 
+<div class="container">
+    <div class="border p-4">
+        <div class="row">
+            <div class="col-sm-6 col-lg-6" id="left-box" style="text-align:center">
+                <img src="{{ $game->image_path }}" class="img-fluid">
+            </div>
+            <div class="col-sm-6 col-lg-6" id="right-box" style="text-align:center">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <p class="font-weight-bold"><th>{{ $game->name }}</th></p>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ $game->describe }}</td>
+                    </tr>
+                     <tr>
+                         <td>{{ $game->play_time }}分</td>
+                     </tr>
+                     <tr>
+                         <td>{{ $game->players_minimum}}~{{ $game->players_max}}人</td>
+                     </th>
+                </tbody>
+            </table>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
