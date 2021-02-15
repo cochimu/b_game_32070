f5a5c007cc569b91944c3644c128af531fef3508
cochimu <cochimu1221@icloud.com>
2021-02-15 11:59:36 +0900

    コントローラー作成、ルーティング設定

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -31,6 +31,7 @@ Route::post('/user/register', [RegisterController::class, 'register'])->name('us
 
 Route::get('/', [GameController::class, 'index']);
 Route::resource('/game', App\Http\Controllers\GameController::class)->only(['create','store','show','edit','update','destroy']);
+Route::resource('game.comments', App\Http\Controllers\CommentController::class);
 
 
 Auth::routes();
