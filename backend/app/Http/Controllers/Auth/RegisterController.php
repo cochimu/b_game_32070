016f4779a838ed5243fa6344e86e1d2ad4adba06
cochimu <cochimu1221@icloud.com>
2021-02-19 19:25:40 +0900

    ルーティング修正

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -65,6 +65,8 @@ class RegisterController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        return redirect()->route('/');
+
+        return redirect('/');
+
     }
 }
